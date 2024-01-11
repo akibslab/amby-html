c7f8e66d989063b4bdeea33341d6c458592b4048
iTanvir <thisistanvir.dev@gmail.com>
2024-01-11 16:49:59 +0600

    Done Mobile Menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,28 @@
 	"use strict";
 
 	$(document).ready(function () {
+		// toolbar active class
 		$(".toolbar_inner .toolbar_item").on("click", function () {
 			$(this).siblings().removeClass("active");
 			$(this).addClass("active");
 		});
+
+		// Header Mobile Menu
+		$(".menu_bar button").on("click", function () {
+			$(".hamburger_menu").addClass("open");
+			$(".page-overly").addClass("open");
+			$("body").addClass("scrollHide");
+		});
+
+		$(".hamburger_menu .close_icon button").on("click", function () {
+			$(".hamburger_menu").removeClass("open");
+			$(".page-overly").removeClass("open");
+			$("body").removeClass("scrollHide");
+		});
+
+		$(".main_menu li.dropdown").on("click", function () {
+			$(this).children("ul").slideToggle();
+		});
+		// end: mobile menu
 	});
 })(jQuery);
